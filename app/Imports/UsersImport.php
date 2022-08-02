e5d94d4f2381f49d7ea9536087777e76a6a4f4dc
pranavpeter2050 <97501797+pranavpeter2050@users.noreply.github.com>
2022-08-03 01:25:38 +0530

    Added code to handle import function.
    Placed the following code inside the app/Imports/UsersImport.php file.

--- app/Imports/UsersImport.php
+++ app/Imports/UsersImport.php
@@ -1,10 +1,10 @@
 <?php
 
 namespace App\Imports;
-
 use App\Models\User;
+use Illuminate\Support\Facades\Hash;
 use Maatwebsite\Excel\Concerns\ToModel;
-
+use Maatwebsite\Excel\Concerns\WithHeadingRow;
 class UsersImport implements ToModel
 {
     /**
@@ -15,7 +15,9 @@ class UsersImport implements ToModel
     public function model(array $row)
     {
         return new User([
-            //
+            'name'     => $row[0],
+            'email'    => $row[1],
+            'password' => Hash::make($row[2])
         ]);
     }
 }
