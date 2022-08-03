047f603c920be1859e56b06679a8b20b24145b1a
pranavpeter2050 <97501797+pranavpeter2050@users.noreply.github.com>
2022-08-03 16:57:40 +0530

    Laravel 9 Vue JS File/Image Upload Related files.
    Refer link: https://www.positronx.io/laravel-vue-js-file-image-upload-example-tutorial/

--- resources/js/app.js
+++ resources/js/app.js
@@ -2,9 +2,12 @@ require('./bootstrap')
 
 import { createApp } from 'vue'
 import HelloWorld from './components/Welcome'
+import FileImport from './components/FileImport'
 
 const app = createApp({})
 
 app.component('hello-world', HelloWorld)
+// app.component('file-import', FileImport)
+app.component('file-upload-component', require('./components/FileImport.vue').default);
 
 app.mount('#app')
