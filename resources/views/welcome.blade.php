fb0a3e7b0f3284ab095a1b4e3ddc6b8d17569dcf
pranavpeter2050 <97501797+pranavpeter2050@users.noreply.github.com>
2022-08-04 16:34:03 +0530

    Changed html structure in base file.

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,17 +1,6 @@
 @extends('layouts.app')
 @section('content')
 <div class="container">
-    <div class="row justify-content-center">
-
-        <div class="col-md-8">
-            <div class="">
-
-                <div class="">
-                  <file-upload-component></file-upload-component>
-                </div>
-
-            </div>
-        </div>
-    </div>
+    <file-upload-component></file-upload-component>
 </div>
 @endsection
