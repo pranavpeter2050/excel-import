672145b000864c39ffa71092672176fb41cdcc62
pranavpeter2050 <97501797+pranavpeter2050@users.noreply.github.com>
2022-08-04 16:30:49 +0530

    The "WithHeadingRow" class is used to read the header of the columns instead of the column-index.
    Ealier data refernced like row[0], i.e. row[column-index]. Now it is referenced like row[column-name].

--- app/Imports/UsersImport.php
+++ app/Imports/UsersImport.php
@@ -5,7 +5,7 @@ use App\Models\User;
 use Illuminate\Support\Facades\Hash;
 use Maatwebsite\Excel\Concerns\ToModel;
 use Maatwebsite\Excel\Concerns\WithHeadingRow;
-class UsersImport implements ToModel
+class UsersImport implements ToModel, WithHeadingRow
 {
     /**
     * @param array $row
@@ -15,9 +15,9 @@ class UsersImport implements ToModel
     public function model(array $row)
     {
         return new User([
-            'name'     => $row[0],
-            'email'    => $row[1],
-            'password' => Hash::make($row[2])
+            'name'     => $row['name'],
+            'email'    => $row['email'],
+            'password' => Hash::make($row['password'])
         ]);
     }
 }
